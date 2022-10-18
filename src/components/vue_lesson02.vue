63d67177fb598cf95add6c486a5180dacb06cc8d
melnikov-vlad <melnikovvladislavzzz@gmail.com>
2022-10-19 00:21:09 +0300

    19.10.2022

---
+++ src/components/vue_lesson02.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'vue_lesson01',
+    props: {
+        msg: String
+    }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import vue_lesson02 from '@/components/vue_lesson02.vue'
 
 export default {
   name: 'HomeView',
