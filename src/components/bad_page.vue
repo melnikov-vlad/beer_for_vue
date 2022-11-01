55718b02861438599b42fc2cd6468f3ae1307709
melnikov-vlad <melnikovvladislavzzz@gmail.com>
2022-11-02 00:46:12 +0200

    kursach/02.11.22

--- src/components/bad_page.vue
+++ src/components/bad_page.vue
@@ -1,38 +1,51 @@
 <template>
     <div class="main_wrapper">
         <h1>Великий світ Ріка та Морті</h1>
+        <img class="logo" src="../assets/image/logo-2.webp" alt="">
         <label for="search">
             <h2>Давай число від 1 до 826 і подивимося який персонаж тобі випаде</h2>
-            <input v-model="numberPerson" class="inp_search" type="text" name="findphrases" id="search" placeholder="Вводи число">
+            <input v-model="numberPerson" class="inp_search" type="text" name="findphrases" id="search"
+                placeholder="Вводи число">
         </label>
         <br>
         <button @click="searchNumberPerson">Почнемо пошук</button>
         <button>Випадкова персонаж</button>
-
-        <div v-for="onePers in persones" :key="persones.id">
+        <!-- if we have single person -->
+        <div class="main_img" v-if="persone.name">
+            <img :src="persone.image" :alt="persone.name">
+            <ul>
+                <li><h3>{{ persone.name }}</h3></li>
+                <li><h4>{{ persone.gender }}</h4></li>
+                <li><h5>{{ persone.status }}</h5></li>
+            </ul>
+            
+        </div>
+        <!-- if we have a list of the persones -->
+        <div v-if="persones.length && !persone.name" v-for="onePers in persones" :key="persones.id">
             <img :src="onePers.image" :alt="onePers.name">
-            <h3>{{ onePers.name }}</h3> 
+            <h3>{{ onePers.name }}</h3>
         </div>
-        <div>
-            <!-- <img :src="persones.image" :alt="persones.name"> -->
-            <!-- <h3>{{ persones.name }}</h3> -->
-        </div>
+        <!-- <div>
+            <img :src="persones.image" :alt="persones.name">
+            <h3>{{ persones.name }}</h3>
+        </div> -->
     </div>
 </template>
 
 <script>
 
 const URL = 'https://rickandmortyapi.com/api/character/';
-
 export default {
     name: 'bad_page',
     components: {
-
     },
-    
+    props: {
+        msg: String
+    },
     data() {
         return {
             persones: [],
+            persone: {},
             numberPerson: '',
             
         }
@@ -42,12 +55,16 @@ export default {
         default: '',
     },
     async mounted() {
-        this.searchNumberPerson()
-        
+        this.searchAllPerson();
     },
     methods: {
         async searchNumberPerson() {
             const res = await fetch(URL + this.numberPerson);
+            const persone = await res.json();
+            this.persone = persone;
+        },
+        async searchAllPerson() {
+            const res = await fetch(URL);
             const persones = await res.json();
             this.persones = persones.results;  
             
@@ -63,4 +80,16 @@ export default {
     padding: 20px;
     margin-bottom: 20px;
 }
+
+.main_img {
+    display: flex;
+    justify-content: center;
+    align-items: center
+}
+
+ul{
+    list-style: none;
+}
+
+
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,65 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="header_nav">
+      <div class="nav_logo">
+        <a href="https://uk.wikipedia.org/wiki/%D0%A0%D1%96%D0%BA_%D1%82%D0%B0_%D0%9C%D0%BE%D1%80%D1%82%D1%96"><img class="logo_img" src="../assets/image/logo-2.webp" alt="#"></a> <!-- Navigation    -->
+      </div>
+      <div>
+        <ul class="nav_list_pages" >
+          <li><a href="#">Пошук героя</a></li>
+          <li><a href="https://www.youtube.com/watch?v=5dvPIdBNbEI&ab_channel=A.K.">Міжгалактичне ТВ</a></li>
+          <li><a href="https://rozetka.com.ua/search/?text=%D1%80%D1%96%D0%BA+%D1%96+%D0%BC%D0%BE%D1%80%D1%82%D1%96">Речі на пам'ять</a></li>
+        </ul>
+      </div>
+      <div>
+        <ul class="nav_list_soc" >
+          <li><a href="https://www.instagram.com/rickandmorty/"><img src="../assets/image/instagram-icon.svg" alt=""></a></li>
+          <li><a href="https://telegram.me/s/Rik_i_Morti2"><img src="../assets/image/telegram-icon.svg" alt=""></a></li>
+          <li><a href="https://www.facebook.com/people/%D0%A0%D0%B8%D0%BA-%D0%B8-%D0%9C%D0%BE%D1%80%D1%82%D0%B8/100069787462769/"><img src="../assets/image/facebook-icon.svg" alt=""></a></li>
+        </ul>
+      </div>
   </div>
 </template>
 
+<script>
+
+</script>
+
+<style scoped lang="scss">
+.header_nav {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  background-image:inherit
+
+}
+.logo_img {
+  width: 100%;
+  max-width: 60px;
+  
+}
+
+ul,
+li {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+.nav_list_pages {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  gap: 20px;
+  text-transform: uppercase;
+}
+
+.nav_list_soc {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  gap: 10px;
+}
+</style>
